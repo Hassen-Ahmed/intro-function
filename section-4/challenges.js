dc023114934ef20ea4df743a0b8ca8c9a099f9d0
Hassen-Ahmed <hassenbest23@gmail.com>
2023-05-20 15:13:29 +0100

    all section-4/ challenge are solved

--- section-4/challenges.js
+++ section-4/challenges.js
@@ -1,103 +1,103 @@
 const { check, runTest, skipTest } = require("../test-api/index.js");
 
 // Exercise 11
-function checkIfPropertyExists() {
+function checkIfPropertyExists(obj, key) {
   // This function should take an object and a key as its arguments and return true if the input object contains the provided key and false otherwise
+  return Object.keys(obj).includes(key);
 }
 
-runTest(
-  "checkIfPropertyExists() checks if a property exists inside an object",
-  function () {
-    check(checkIfPropertyExists({ name: "jonny", age: 32 }, "name")).isEqualTo(
-      true
-    );
-    check(checkIfPropertyExists({ name: "jonny", age: 32 }, "age")).isEqualTo(
-      true
-    );
-    check(checkIfPropertyExists({ name: "jonny", age: 32 }, "pets")).isEqualTo(
-      false
-    );
-  }
-);
+runTest("checkIfPropertyExists() checks if a property exists inside an object", function () {
+  check(checkIfPropertyExists({ name: "jonny", age: 32 }, "name")).isEqualTo(true);
+  check(checkIfPropertyExists({ name: "jonny", age: 32 }, "age")).isEqualTo(true);
+  check(checkIfPropertyExists({ name: "jonny", age: 32 }, "pets")).isEqualTo(false);
+});
 
 // Exercise 12
-function createObject() {
+function createObject(arr) {
   // This function should take an array consisting of two elements representing a key/ value pair as its argument and return an object with a single property based on the input
+
+  const objectLiteral = {};
+  objectLiteral[arr[0]] = arr[1];
+  return objectLiteral;
 }
 
-skipTest(
-  "createObject() creates a new object from a key value pair",
-  function () {
-    check(createObject(["name", "shaq"])).isEqualTo({ name: "shaq" });
-    check(createObject(["fruit", "apple"])).isEqualTo({ fruit: "apple" });
-    check(createObject(["language", "haskell"])).isEqualTo({
-      language: "haskell"
-    });
-  }
-);
+runTest("createObject() creates a new object from a key value pair", function () {
+  check(createObject(["name", "shaq"])).isEqualTo({ name: "shaq" });
+  check(createObject(["fruit", "apple"])).isEqualTo({ fruit: "apple" });
+  check(createObject(["language", "haskell"])).isEqualTo({
+    language: "haskell",
+  });
+});
 
 // Exercise 13
-function getFirstNItems() {
+function getFirstNItems(arr, n) {
   // This function should take two arguments, an array and a number 'n', and return a new array containing the first 'n' items of the given array
+
+  if (arr.length === 0) [];
+  return arr.slice(0, n);
 }
 
-skipTest("getFirstNItems() returns the first n items in an array", function () {
+runTest("getFirstNItems() returns the first n items in an array", function () {
   check(getFirstNItems(["a", "b", "c", "d"], 2)).isEqualTo(["a", "b"]);
   check(getFirstNItems(["apple", "banana", "pear", "kiwi"], 0)).isEqualTo([]);
   check(getFirstNItems(["apple", "banana", "pear", "kiwi"], 3)).isEqualTo([
     "apple",
     "banana",
-    "pear"
+    "pear",
   ]);
 });
 
 // Exercise 14
-function createArrow() {
+function createArrow(str) {
   // This function should take a string representing a direction ("left", "right", "up" or "down") as its argument and return the corresponding arrow ("←", "→", "↑", "↓")
   // You don't need to utilise an object here, but think about how you could do so
+  let direction = "";
+  switch (str) {
+    case "left":
+      direction = "←";
+      break;
+    case "right":
+      direction = "→";
+      break;
+    case "up":
+      direction = "↑";
+      break;
+    case "down":
+      direction = "↓";
+      break;
+
+    default:
+      break;
+  }
+  return direction;
 }
 
-skipTest(
-  "createArrow() will return an arrow pointing in the right direction",
-  function () {
-    check(createArrow("left")).isEqualTo("←");
-    check(createArrow("right")).isEqualTo("→");
-    check(createArrow("up")).isEqualTo("↑");
-    check(createArrow("down")).isEqualTo("↓");
-  }
-);
+runTest("createArrow() will return an arrow pointing in the right direction", function () {
+  check(createArrow("left")).isEqualTo("←");
+  check(createArrow("right")).isEqualTo("→");
+  check(createArrow("up")).isEqualTo("↑");
+  check(createArrow("down")).isEqualTo("↓");
+});
 
 // Exercise 15
-function moveItemToEnd() {
+function moveItemToEnd(arr, index) {
   // This function should take two arguments, an array and an index value, and should return a new array where the item that was previously at the given index is now at the end of the array
+
+  let endLetter = arr.splice(index, 1);
+  return [...arr, ...endLetter];
 }
 
-skipTest(
+runTest(
   "moveItemToEnd() removes an item at a given index and adds it to the end of the array",
   function () {
-    check(moveItemToEnd(["a", "b", "c", "d"], 0)).isEqualTo([
-      "b",
-      "c",
-      "d",
-      "a"
-    ]);
-    check(moveItemToEnd(["a", "b", "c", "d"], 2)).isEqualTo([
-      "a",
-      "b",
-      "d",
-      "c"
-    ]);
-    check(moveItemToEnd(["a", "b", "c", "d"], 1)).isEqualTo([
-      "a",
-      "c",
-      "d",
-      "b"
-    ]);
+    check(moveItemToEnd(["a", "b", "c", "d"], 0)).isEqualTo(["b", "c", "d", "a"]);
+    check(moveItemToEnd(["a", "b", "c", "d"], 2)).isEqualTo(["a", "b", "d", "c"]);
+    check(moveItemToEnd(["a", "b", "c", "d"], 1)).isEqualTo(["a", "c", "d", "b"]);
   }
 );
 
 // Exercise 16
-function updateUserAge() {
+function updateUserAge(userAccount) {
   /*
   The user of our website is having a birthday!
 
@@ -117,17 +117,18 @@ function updateUserAge() {
 
   NOTE: This function does NOT need to return anything!
   */
+  userAccount.personalDetails.age += 1;
 }
 
-skipTest("updateUserAge() updates the user's age", function () {
+runTest("updateUserAge() updates the user's age", function () {
   const user1 = {
     admin: false,
     username: "xoxoAlexoxo",
     personalDetails: {
       name: "Alex",
       age: 39,
-      favFood: "gooseberry fool"
-    }
+      favFood: "gooseberry fool",
+    },
   };
 
   updateUserAge(user1);
@@ -138,8 +139,8 @@ skipTest("updateUserAge() updates the user's age", function () {
     personalDetails: {
       name: "Alex",
       age: 40,
-      favFood: "gooseberry fool"
-    }
+      favFood: "gooseberry fool",
+    },
   });
 
   const user2 = {
@@ -148,8 +149,8 @@ skipTest("updateUserAge() updates the user's age", function () {
     personalDetails: {
       name: "Poonam",
       age: 19,
-      favFood: "caviar"
-    }
+      favFood: "caviar",
+    },
   };
 
   updateUserAge(user2);
@@ -160,83 +161,75 @@ skipTest("updateUserAge() updates the user's age", function () {
     personalDetails: {
       name: "Poonam",
       age: 20,
-      favFood: "caviar"
-    }
+      favFood: "caviar",
+    },
   });
 });
 
 // Exercise 17
-function checkInfinitive() {
+function checkInfinitive(word) {
   // This function should take a string representing a French word as an argument, and return true if it is an infinitive verb, and false otherwise
   // A French infinitive verb is a word that ends with either "re", "ir" or "er"
+  return /\b.*(re|ir|er)\b/g.test(word);
 }
 
-skipTest(
-  "checkInfinitive() checks if a french word is an infinitive",
-  function () {
-    check(checkInfinitive("manger")).isEqualTo(true);
-    check(checkInfinitive("faire")).isEqualTo(true);
-    check(checkInfinitive("aller")).isEqualTo(true);
-    check(checkInfinitive("finir")).isEqualTo(true);
-    check(checkInfinitive("rendre")).isEqualTo(true);
-    check(checkInfinitive("savoir")).isEqualTo(true);
+runTest("checkInfinitive() checks if a french word is an infinitive", function () {
+  check(checkInfinitive("manger")).isEqualTo(true);
+  check(checkInfinitive("faire")).isEqualTo(true);
+  check(checkInfinitive("aller")).isEqualTo(true);
+  check(checkInfinitive("finir")).isEqualTo(true);
+  check(checkInfinitive("rendre")).isEqualTo(true);
+  check(checkInfinitive("savoir")).isEqualTo(true);
 
-    check(checkInfinitive("suis")).isEqualTo(false);
-    check(checkInfinitive("ai")).isEqualTo(false);
-    check(checkInfinitive("ete")).isEqualTo(false);
-    check(checkInfinitive("sais")).isEqualTo(false);
-    check(checkInfinitive("allons")).isEqualTo(false);
-  }
-);
+  check(checkInfinitive("suis")).isEqualTo(false);
+  check(checkInfinitive("ai")).isEqualTo(false);
+  check(checkInfinitive("ete")).isEqualTo(false);
+  check(checkInfinitive("sais")).isEqualTo(false);
+  check(checkInfinitive("allons")).isEqualTo(false);
+});
 
 // Exercise 18
-function collectPlurals() {
+function collectPlurals(strings) {
   // This function should take an array of strings as an argument and return an array containing all strings ending with an 's' from the input (retaining the order)
+  return strings.filter((string) => /\b.*s$/g.test(string));
 }
 
-skipTest(
-  "collectPlurals() can collect all the strings ending in an s",
-  function () {
-    check(
-      collectPlurals(["dogs", "cat", "apples", "kittens", "kiwi"])
-    ).isEqualTo(["dogs", "apples", "kittens"]);
+runTest("collectPlurals() can collect all the strings ending in an s", function () {
+  check(collectPlurals(["dogs", "cat", "apples", "kittens", "kiwi"])).isEqualTo([
+    "dogs",
+    "apples",
+    "kittens",
+  ]);
 
-    check(
-      collectPlurals([
-        "abcs",
-        "humans",
-        "thoughts",
-        "cloud",
-        "computer",
-        "cups"
-      ])
-    ).isEqualTo(["abcs", "humans", "thoughts", "cups"]);
-  }
-);
+  check(collectPlurals(["abcs", "humans", "thoughts", "cloud", "computer", "cups"])).isEqualTo([
+    "abcs",
+    "humans",
+    "thoughts",
+    "cups",
+  ]);
+});
 
 // Exercise 19
-function makeAllAdmins() {
+function makeAllAdmins(usersArray) {
   /*
   This function should take an array of 'user' objects as an argument
   Each user will be an object with a 'name' and 'admin' property
   The 'admin' property will be a boolean value
   You should return an array of user objects each with the 'admin' property set to true
   */
+  return usersArray.map((user) => ({ ...user, admin: true }));
 }
 
-skipTest(
-  "makeAllAdmins() updates the admin property for each user",
-  function () {
-    check(
-      makeAllAdmins([
-        { name: "Barry", admin: false },
-        { name: "Sandeep", admin: true },
-        { name: "Kavita", admin: false }
-      ])
-    ).isEqualTo([
-      { name: "Barry", admin: true },
+runTest("makeAllAdmins() updates the admin property for each user", function () {
+  check(
+    makeAllAdmins([
+      { name: "Barry", admin: false },
       { name: "Sandeep", admin: true },
-      { name: "Kavita", admin: true }
-    ]);
-  }
-);
+      { name: "Kavita", admin: false },
+    ])
+  ).isEqualTo([
+    { name: "Barry", admin: true },
+    { name: "Sandeep", admin: true },
+    { name: "Kavita", admin: true },
+  ]);
+});
